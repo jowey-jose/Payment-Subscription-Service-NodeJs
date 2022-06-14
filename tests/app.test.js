0ca660e238f46975fb4046763642f9c562a57142
Jowey-jose <josephmbuku17@gmail.com>
2022-06-14 18:36:26 +0300

    Adding Unit Test To user Services

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,25 @@
+// First Test Case To test Saving of a User tO DB
+const { addUser } = require("../api/controllers/userController"); // Importing the save user function.
+
 describe("User Service Unit Tests", function () {
-    describe("Save User functionality", function () {
-      it("should successfully add a user if the number of users in the DB with the same billingID is zero", async function () {
+  describe("Add User functionality", function () {
+    it("should successfully add a user if the number of users in the DB with the same profiled is zero", async function () {
+      const email = "joe@gmail.com";
+      const billingID = "#123456";
+      const plan = "basic";
+      const endDate = "2022--12";
+      const returnedUser = await addUser({
+        email,
+        billingID,
+        plan,
+        endDate,
       });
-      it("should throw an error if the number of users with the same billingID is not zero", async function () {
-      });
+      expect(returnedUser.email).to.equal(email);
+      expect(returnedUser.endDate.toString()).to.equal((new Date(endDate)).toString());
+      expect(returnedUser.email).to.equal(email);
+      expect(returnedUser.billingID).to.equal(billingID);
+      expect(returnedUser.plan).to.equal(plan);
     });
+    it("should throw an error if the number of users with the same billingID is not zero", async function () {});
   });
+});
